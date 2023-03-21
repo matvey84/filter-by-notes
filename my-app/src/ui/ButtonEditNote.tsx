4794c7549cffe156f39405677f4f1397c704acef
matvey84 <matvey8407@mail.ru>
2023-03-21 14:16:35 +0300

    refactor: change className

--- my-app/src/ui/ButtonEditNote.tsx
+++ my-app/src/ui/ButtonEditNote.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { useAppDispatch } from '../redux/hooks';
 import { EditTaskSVG } from './svgButtons';
-import './task-buttons.css';
+import './buttons.css';
 
 interface IProp {
   id: string;
@@ -10,17 +9,10 @@ interface IProp {
 }
 export const ButtonEditNote = (props: IProp) => {
   const { id, setIsEdit, isEdit } = props;
-  const dispatch = useAppDispatch();
-  const goToModalWindow = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // dispatch(setIsEditTask(true));
-    // dispatch(setIsShowTask(false));
-    // dispatch(setModalOpen(true));
-    // dispatch(setCurrentTaskId(e.currentTarget.id));
-    // dispatch(setCurrentColumnId(props.column.id));
-  };
+
   return (
     <>
-      <button id={id} className="button-edit-task" onClick={() => setIsEdit(!isEdit)}>
+      <button id={id} className="button-edit-note" onClick={() => setIsEdit(!isEdit)}>
         <EditTaskSVG />
       </button>
     </>
